fb79bf5966381dddd3bde725c1883c670dacee21
Varalakshmi Yelisetty <varaluy2006@gmail.com>
2026-06-15 16:19:29 +0530

    Completed React Counter App using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,50 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+
+  if (action.type === "reset") {
+    return { count: 0 };
+  }
+
+  return state;
+}
 
 function App() {
 
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-
-    setInterval(() => {
-
-      setTime(new Date());
-
-    }, 1000);
-
-  }, []);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0
+  });
 
   return (
-
     <div>
 
-      <h1>
-        {time.toLocaleTimeString()}
-      </h1>
+      <h1>{state.count}</h1>
+
+      <button
+        onClick={() => dispatch({ type: "increment" })}
+      >
+        Increment
+      </button>
+
+      <button
+        onClick={() => dispatch({ type: "decrement" })}
+      >
+        Decrement
+      </button>
+
+      <button
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Reset
+      </button>
 
     </div>
   );
